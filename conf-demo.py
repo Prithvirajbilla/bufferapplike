0c455fbf6156fd1f04f176ff55f402ff7014d30a
prithvirajbilla <prithviraj.m.billa@gmail.com>
2014-02-09 04:00:10 +0530

    working mail.py

--- conf-demo.py
+++ conf-demo.py
@@ -1,4 +1,4 @@
-X-Mashape-Authorization = "xxxxxxxxxxxxxxxxxxxxxxx"
-ToMail = "trigger@ifttt.com"
-FromMail = "youremail@mail.com"
-Password = " "
+XMashapeAuthorization = "xxxxxxxxxxxxxxxxxxxxxxx"
+mailgunkey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+TO_MAIL = "trigger@ifttt.com"
+FROM_MAIL = "youremail@mail.com"
---
+++ email.py
@@ -0,0 +1,12 @@
+from conf import *
+import requests
+def send_simple_message(subject, content):
+    return requests.post(
+            "https://api.mailgun.net/v2/sandbox7930.mailgun.org/messages",
+            auth=("api", mailgunkey),
+            data={"from": FROM_MAIL,
+                  "to": TO_MAIL,
+                  "subject": subject,
+                  "text": content})
+
+print send_simple_message("This is good","good")
