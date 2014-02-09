2c88d4b038585923fe974b91e13a0403765e7634
prithvirajbilla <prithviraj.m.billa@gmail.com>
2014-02-09 18:08:55 +0530

    added get_files fnct:

--- buffercron.py
+++ buffercron.py
@@ -1,5 +1,17 @@
 from email import send_simple_message
 from conf import *
+import glob
+import os
+def get_files(directory):
+	os.chdir(directory)
+	lsfiles = glob.glob("*.txt")
+	return lsfiles
+
+def get_now_files(files):
+	pass
+
 
 def buffer_post(directory):
-	pass
+	files = get_files(directory)
+	real_files = get_now_files(files)
+
