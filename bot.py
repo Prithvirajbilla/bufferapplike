3bf767f50a3f0e60afb9ac32e4c25bb7942e6154
prithvirajbilla <prithviraj.m.billa@gmail.com>
2014-02-09 17:52:36 +0530

    Added things to the bot :+1: and :dancer:

---
+++ bot.py
@@ -0,0 +1,46 @@
+from conf import *
+from hackernews import *
+
+def auto_post():
+	hn = HackerNews(key=XMashapeAuthorization)
+
+	js = hn.getResult("best")
+	if js:
+		for i in range(5):
+			try:
+				message = js[i]["title"]+"\n"
+				message+= "Comments link: "+js[i]["comments_link"]+"\n"
+				message+= "Published "+ js[i]["published_time"]+"\n"
+				message+= "Category:Best Links"
+				subject = js[i]["link"]
+				print message,subject
+			except Exception,e:
+				pass
+
+	js = hn.getResult("top")
+	if js:
+		for i in range(5):
+			try:
+				message = js[i]["title"]+"\n"
+				message+= "Comments link: "+js[i]["comments_link"]+"\n"
+				message+= "Published "+ js[i]["published_time"]+"\n"
+				message+= "Category:Top links"
+				subject = js[i]["link"]
+				print message,subject
+			except Exception,e:
+				pass
+
+	js = hn.getResult("newest")
+	if js:
+		for i in range(5):
+			try:
+				message = js[i]["title"]+"\n"
+				message+= "Comments link: "+js[i]["comments_link"]+"\n"
+				message+= "Published "+ js[i]["published_time"]+"\n"
+				message+= "Category:Newest links"
+				subject = js[i]["link"]
+				print message,subject
+			except Exception,e:
+				pass
+
+auto_post()
--- hackernews.py
+++ hackernews.py
@@ -3,13 +3,13 @@ import json
 
 class HackerNews:
 	url = "https://community-hnify.p.mashape.com/"
-	def __init__(self,key)
+	def __init__(self,key):
 		self.key = key
 	
 	def getResult(self,which):
 		extra = "get/"+which
 		r=requests.get(self.url+extra, headers={"X-Mashape-Authorization":self.key});
-		if r.status_code = 200:
+		if r.status_code == 200:
 			try:
 				return r.json()["stories"]
 			except Exception,e:
